ac34f4fea647a6230bc9ce9e0aaeffd852cb0e76
Siddhant Yadav <sidhantyadav1999@gmail.com>
2026-04-26 22:17:33 +0530

    first commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Siddhant Yadav on 26/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
